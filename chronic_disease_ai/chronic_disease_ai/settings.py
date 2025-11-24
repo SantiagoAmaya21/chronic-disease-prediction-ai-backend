58b1c2f1d895872765e7db4654a918a7bcc3f3c9
Laura Natalia Perilla Quintero <laura.perilla-q@mail.escuelaing.edu.co>
2025-11-23 22:34:54 -0500

    feat(config): add CORS_ALLOW_ALL_ORIGINS in settings.py to enable frontend access

--- chronic_disease_ai/chronic_disease_ai/settings.py
+++ chronic_disease_ai/chronic_disease_ai/settings.py
@@ -40,6 +40,7 @@ INSTALLED_APPS = [
     'api',
     'rest_framework',
     'django_extensions',
+    "corsheaders"
 ]
 
 MIDDLEWARE = [
@@ -50,8 +51,11 @@ MIDDLEWARE = [
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
+    "corsheaders.middleware.CorsMiddleware",
+    "django.middleware.common.CommonMiddleware"
 ]
 
+CORS_ALLOW_ALL_ORIGINS = True
 ROOT_URLCONF = 'chronic_disease_ai.urls'
 
 TEMPLATES = [
